c8d2d558b9165f05fadf636b14cb486a09727f39
Roman Teplov <teplov@gmail.com>
2021-10-02 14:40:42 +0300

    First

---
+++ client.js
@@ -0,0 +1,25 @@
+
+document.addEventListener("DOMContentLoaded", init);
+
+
+var id = null;
+
+function init() {
+    const myWs = new WebSocket('ws://id-wschat.herokuapp.com');
+    console.log("Loaded")
+
+    document.getElementById('input').addEventListener("keypress", (e) => {
+        if (e.code == 'Enter') {
+            let val = e.target.value;
+            e.target.value = null;
+            console.log(val);
+            const data = {
+                client_id: id,
+                client_message: val
+            };
+            myWs.send(JSON.stringify({action: 'client_send_message', data}));    
+        }
+    });
+
+    window.addEventListener("message", (e) => e.data ? id = e.data : id = null);
+}
