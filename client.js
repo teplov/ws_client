6abb3218fec2147bad7add76d84f49f44fe70c1c
Roman Teplov <teplov@gmail.com>
2021-10-02 15:35:22 +0300

    messages

--- client.js
+++ client.js
@@ -4,6 +4,17 @@ document.addEventListener("DOMContentLoaded", init);
 
 var id = null;
 
+const addMessage = (message, type='out') => {
+
+    const mesEl = document.createElement("div");
+    mesEl.classList.add("message", type);
+    mesEl.innerText = message;
+
+    const mainEl = document.getElementById('main');
+    mainEl.appendChild(mesEl);
+    mainEl.scrollTop = 10000;
+}
+
 function init() {
     const myWs = new WebSocket('wss://id-wschat.herokuapp.com');
     console.log("Loaded")
@@ -13,6 +24,7 @@ function init() {
             let val = e.target.value;
             e.target.value = null;
             console.log(val);
+            addMessage(val);
             const data = {
                 client_id: id,
                 client_message: val
@@ -23,6 +35,7 @@ function init() {
 
     myWs.onmessage = function (message) {
         console.log('Message: %s', message.data);
+        addMessage(message.data, "in");
       };
 
     window.addEventListener("message", (e) => e.data ? id = e.data : id = null);
